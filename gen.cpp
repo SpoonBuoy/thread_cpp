d981844a1a4e039a7e48e1f513f2a01f2b7be066
Arsalan Rashid <arsalanrashidthoker@gmail.com>
2024-05-01 23:29:10 +0530

    make test gen concurrent

--- gen.cpp
+++ gen.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <fstream>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
 
 int main() {
     printf("creating test files \n");
+    vector<thread> threads;
     for(int i=1;i<=100;i++){
-        fstream file;
-        string fname = to_string(i) + ".txt";
-        file.open(fname, ios::out);
-        if(file) {
+        threads.push_back(thread([i]{
+          fstream file;
+          string fname = to_string(i) + ".txt";
+          file.open(fname, ios::out);
+          if(file) {
             for(int j=1;j<=1000;j++) {
                 file<<j<<endl;
             }
-        }
+         }
+        }));
+    }
+    for(auto &t:threads) {
+        t.join();
     }
     printf("creating test files done");
     return 0;
